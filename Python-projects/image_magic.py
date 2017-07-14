5e0bc01a77c899847d2a45d835d4f2d7e3c13422
zl <zl03jsj@163.com>
2017-07-14 17:51:49 +0800

    do_image_magic function, but there is something wrong need to reslove

--- Python-projects/image_magic.py
+++ Python-projects/image_magic.py
@@ -15,14 +15,10 @@ print sharpness_filter_5_5
 
 
 def multiple_sum(f, sub_img):
-    print "-------------------"
-    print f
-    print sub_img
     r = (f * sub_img[:, :, 0]).sum()
     g = (f * sub_img[:, :, 1]).sum()
     b = (f * sub_img[:, :, 2]).sum()
     color = np.array([r, g, b], dtype=np.uint8)
-    print "-------------------"
     return color
 
 
@@ -32,51 +28,54 @@ def do_image_magic(img, f):
     print 'img shape = ', img.shape
     print 'filter cub shape = ', f.shape
 
-    fx = f.shape[0]
-    fy = f.shape[1]
-
-    if 0 == (fx % 2) or 0 == (fy % 2):
+    if f.shape[0]!=f.shape[1] or 0==(f.shape[0] % 2):
         print "filter cub shape can not be even,should be odd number!!!!"
         return
 
-    fx /= 2
-    fy /= 2
+    r = f.shape[0]/2
 
-    tmp = np.arange(0, img.shape[1], dtype=object)
+    tmp = np.zeros((img.shape[1] * (r+1), 3), dtype=np.uint8).reshape(r+1, img.shape[1], 3)
+    print tmp.shape
+    tx = 0
+
     for x in np.arange(0, img.shape[0]):
-        if x > 0:
-            img[0, 0:img.shape[1]] = tmp
-
-        begin_x, end_x = x-fx, x+fx
+        begin_x, end_x = x-r, x+r
 
         if begin_x < 0: begin_x = 0
-        if end_x > img.shape[0]: end_x = img.shape[0]
+        if end_x >= img.shape[0]: end_x = img.shape[0] - 1
 
         begin_fx = f.shape[0] - (end_x - begin_x + 1)
         end_fx = begin_fx + (end_x - begin_x + 1)
 
+        tx = x if x <= r else r
         for y in np.arange(0, img.shape[1]):
-            begin_y, end_y = y-fy, y+fy
+            begin_y, end_y = y-r, y+r
             if begin_y < 0: begin_y = 0
-            if end_y > img.shape[1]: end_y = img.shape[y]
+            if end_y >= img.shape[1]: end_y = img.shape[1] - 1
 
             begin_fy = f.shape[1] - (end_y - begin_y + 1)
             end_fy = begin_fy + (end_y - begin_y + 1)
 
-            print "filter range:([%d-%d], [%d-%d]), img range:([%d-%d], [%d-%d])" % (begin_fx, end_fx, begin_fy, end_fy, begin_x, end_x, begin_y, end_y)
-
             sub_filter = f[begin_fx:end_fx + 1, begin_fy:end_fy + 1]
             sub_img = img[begin_x:end_x + 1, begin_y:end_y + 1]
 
-            tmp[y] = multiple_sum(sub_filter, sub_img)
+            tmp[tx, y, :] = multiple_sum(sub_filter, sub_img)
+
+        if x >= r:
+            img[x-r, 0:img.shape[1], :] = tmp[0, 0:tmp.shape[1], :]
+            for i in [1, r]:
+                tmp[i-1, 0:tmp.shape[1], :] = tmp[i, 0:tmp.shape[1], :]
+
+    for x in np.arange(img.shape[0]-r, img.shape[0]):
+        img[x, 0:img.shape[1], :] = tmp[img.shape[0]-x, 0:tmp.shape[1], :]
+
     print '-----------do_image_magic-----------'
 
-imagefile = "bird.jpg"
+imagefile = "snipaste_20170714_162153.jpg"
 birdimage = cv2.imread(imagefile)
 
 do_image_magic(birdimage, sharpness_filter_3_3)
 
-
 cv2.imshow("image magic show!!!!", birdimage)
-cv2.waitKey(6*1000)
+cv2.waitKey()
 cv2.destroyAllWindows()
