48b53cdc0a6cae8f08794a7aba15fc1ec6166c28
zl <zl03jsj@163.com>
2017-07-15 19:18:38 +0800

    Convolution function test ok, in image_maic.py

--- Python-projects/image_magic.py
+++ Python-projects/image_magic.py
@@ -2,10 +2,13 @@
 import cv2
 import numpy as np
 
-
 # 图像锐化滤波器Sharpness Filter
+sharpness_filter_1_1 = np.linspace(0, 0, 9).reshape(3, 3)
+sharpness_filter_1_1[1, 1] = 1
+print sharpness_filter_1_1
+
 sharpness_filter_3_3 = np.linspace(-1, -1, 9).reshape(3, 3)
-sharpness_filter_3_3[1, 1] = 9
+sharpness_filter_3_3[1, 1] = 8
 print sharpness_filter_3_3
 
 sharpness_filter_5_5 = np.linspace(-1, -1, 25).reshape(5, 5)
@@ -13,11 +16,10 @@ sharpness_filter_5_5[1:4, 1:4] = 2
 sharpness_filter_5_5[2, 2] = 8
 print sharpness_filter_5_5
 
-
 def multiple_sum(f, sub_img):
-    r = (f * sub_img[:, :, 0]).sum()
-    g = (f * sub_img[:, :, 1]).sum()
-    b = (f * sub_img[:, :, 2]).sum()
+    r = min(abs((f * sub_img[:, :, 0]).sum()), 255)
+    g = min(abs((f * sub_img[:, :, 1]).sum()), 255)
+    b = min(abs((f * sub_img[:, :, 2]).sum()), 255)
     color = np.array([r, g, b], dtype=np.uint8)
     return color
 
@@ -34,45 +36,47 @@ def do_image_magic(img, f):
 
     r = f.shape[0]/2
 
-    tmp = np.zeros((img.shape[1] * (r+1), 3), dtype=np.uint8).reshape(r+1, img.shape[1], 3)
-    print tmp.shape
+    tmp = np.zeros((img.shape[0] * (r+1), 3), dtype=np.uint8).reshape(img.shape[0], r+1,  3)
     tx = 0
 
-    for x in np.arange(0, img.shape[0]):
-        begin_x, end_x = x-r, x+r
+    for x in np.arange(0, img.shape[1]):
+        begin_x = max(0, x-r)
+        end_x = min(img.shape[1]-1, x+r)
 
-        if begin_x < 0: begin_x = 0
-        if end_x >= img.shape[0]: end_x = img.shape[0] - 1
-
-        begin_fx = f.shape[0] - (end_x - begin_x + 1)
-        end_fx = begin_fx + (end_x - begin_x + 1)
+        begin_fx = max(0, r-x)
+        end_fx = min(f.shape[1]-1, (img.shape[1] - x))
 
         tx = x if x <= r else r
-        for y in np.arange(0, img.shape[1]):
-            begin_y, end_y = y-r, y+r
-            if begin_y < 0: begin_y = 0
-            if end_y >= img.shape[1]: end_y = img.shape[1] - 1
+        print "x = %d, tx=%d" %(x, tx)
 
-            begin_fy = f.shape[1] - (end_y - begin_y + 1)
-            end_fy = begin_fy + (end_y - begin_y + 1)
+        for y in np.arange(0, img.shape[0]):
+            begin_y = max(0, y-r)
+            end_y = min(img.shape[1]-1, y+r)
 
-            sub_filter = f[begin_fx:end_fx + 1, begin_fy:end_fy + 1]
-            sub_img = img[begin_x:end_x + 1, begin_y:end_y + 1]
+            begin_fy = max(0, r-y)
+            end_fy = min(f.shape[0]-1, (img.shape[0] - y))
 
-            tmp[tx, y, :] = multiple_sum(sub_filter, sub_img)
+            sub_filter = f[begin_fy:end_fy + 1, begin_fx:end_fx + 1]
+            sub_img = img[begin_y:end_y + 1, begin_x:end_x + 1]
+
+            tmp[y, tx, :] = multiple_sum(sub_filter, sub_img)
 
         if x >= r:
-            img[x-r, 0:img.shape[1], :] = tmp[0, 0:tmp.shape[1], :]
-            for i in [1, r]:
-                tmp[i-1, 0:tmp.shape[1], :] = tmp[i, 0:tmp.shape[1], :]
+            print "copy pixels from (%d:%d, %d) to img(%d:%d, %d)" %(0, tmp.shape[0], 0, 0, img.shape[0], x-r)
+            img[0:img.shape[0], x-r, :] = tmp[0:tmp.shape[0], 0, :]
+            for i in np.arange(1, r+1):
+                tmp[0:tmp.shape[0], i-1, :] = tmp[0:tmp.shape[0], i, :]
 
-    for x in np.arange(img.shape[0]-r, img.shape[0]):
-        img[x, 0:img.shape[1], :] = tmp[img.shape[0]-x, 0:tmp.shape[1], :]
+    for x in np.arange(img.shape[1]-r, img.shape[1]):
+        print "copy pixels from (%d:%d, %d) to img(%d:%d, %d)" %(0, tmp.shape[0], img.shape[1]-x, 0, img.shape[0], x)
+        img[0:img.shape[0], x, :] = tmp[0:tmp.shape[0], img.shape[1]-x, :]
 
     print '-----------do_image_magic-----------'
 
-imagefile = "snipaste_20170714_162153.jpg"
+imagefile = "flower.jpg"
 birdimage = cv2.imread(imagefile)
+
+cv2.imshow("original image", birdimage)
 
 do_image_magic(birdimage, sharpness_filter_3_3)
 
