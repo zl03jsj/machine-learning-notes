a9025c2371d809b32d4fdbf0fce33614550ee7cc
zl <zl03jsj@163.com>
2017-07-17 00:05:27 +0800

    all Convolution test ok

--- Python-projects/image_magic.py
+++ Python-projects/image_magic.py
@@ -3,23 +3,65 @@ import cv2
 import numpy as np
 
 # 图像锐化滤波器Sharpness Filter
-sharpness_filter_1_1 = np.linspace(0, 0, 9).reshape(3, 3)
-sharpness_filter_1_1[1, 1] = 1
-print sharpness_filter_1_1
+sharpness_filter_1 = np.linspace(0, 0, 25).reshape(5, 5)
+sharpness_filter_1[3, 3] = 1
 
-sharpness_filter_3_3 = np.linspace(-1, -1, 9).reshape(3, 3)
-sharpness_filter_3_3[1, 1] = 8
-print sharpness_filter_3_3
+sharpness_filter_2 = np.linspace(-1, -1, 9).reshape(3, 3)
+sharpness_filter_2[1, 1] = 9
 
-sharpness_filter_5_5 = np.linspace(-1, -1, 25).reshape(5, 5)
-sharpness_filter_5_5[1:4, 1:4] = 2
-sharpness_filter_5_5[2, 2] = 8
-print sharpness_filter_5_5
+sharpness_filter_3 = np.linspace(1, 1, 9).reshape(3, 3)
+sharpness_filter_3[1, 1] = -7
+
+sharpness_filter_4 = np.linspace(-1, -1, 25).reshape(5, 5)
+sharpness_filter_4[1:4, 1:4] = 2
+sharpness_filter_4[2, 2] = 1
+
+sharpness_filter_5 = np.zeros(25).reshape(5, 5)
+sharpness_filter_5[2, 0:3] = [-1, -1, 2]
+
+sharpness_filter_6 = np.zeros(25).reshape(5, 5)
+sharpness_filter_6[0:5, 2] = [-1, -1, 4, -1, -1]
+
+sharpness_filter_7 = np.zeros(25).reshape(5, 5)
+sharpness_filter_7[0, 0] = -1
+sharpness_filter_7[1, 1] = -2
+sharpness_filter_7[2, 2] = 6
+sharpness_filter_7[3, 3] = -2
+sharpness_filter_7[4, 4] = -1
+
+sharpness_filter_8 = np.linspace(-1, -1, 9).reshape(3, 3)
+sharpness_filter_8[1, 1] = 8
+
+sharpness_filter_9 = np.array([[-1, -1, 0],
+                               [-1,  0, 1],
+                               [ 0,  1, 1]])
+
+sharpness_filter_10 = np.array([[-1, -1, -1, -1, 0],
+                                [-1, -1, -1,  0, 1],
+                                [-1, -1,  0,  1, 1],
+                                [-1,  0,  1,  1, 1],
+                                [ 0,  1,  1,  1, 1]])
+
+sharpness_filter_11 = np.linspace(0.04, 0.04, 25).reshape(5, 5)
+
+#need to divied 273
+sharpness_filter_12 = np.array([[1.0/273,  4.0/273,  7.0/273,  4.0/273, 1.0/273],
+                                [4.0/273, 16.0/273, 26.0/273, 16.0/273, 4.0/273],
+                                [7.0/273, 26.0/273, 41.0/273, 26.0/273, 7.0/273],
+                                [4.0/273, 16.0/273, 26.0/273, 16.0/273, 4.0/273],
+                                [1.0/273,  4.0/273,  7.0/273,  4.0/273, 1.0/273]])
+
+
+
+filters = [sharpness_filter_1, sharpness_filter_2, sharpness_filter_3,
+           sharpness_filter_4, sharpness_filter_5,  sharpness_filter_6,
+           sharpness_filter_7, sharpness_filter_8,  sharpness_filter_9,
+           sharpness_filter_10, sharpness_filter_11,  sharpness_filter_12]
 
 def multiple_sum(f, sub_img):
-    r = min(abs((f * sub_img[:, :, 0]).sum()), 255)
-    g = min(abs((f * sub_img[:, :, 1]).sum()), 255)
-    b = min(abs((f * sub_img[:, :, 2]).sum()), 255)
+    r = min(max(0, (f * sub_img[:, :, 0]).sum()), 255)
+    g = min(max(0, (f * sub_img[:, :, 1]).sum()), 255)
+    b = min(max(0, (f * sub_img[:, :, 2]).sum()), 255)
     color = np.array([r, g, b], dtype=np.uint8)
     return color
 
@@ -29,6 +71,7 @@ def do_image_magic(img, f):
 
     print 'img shape = ', img.shape
     print 'filter cub shape = ', f.shape
+    print  f
 
     if f.shape[0]!=f.shape[1] or 0==(f.shape[0] % 2):
         print "filter cub shape can not be even,should be odd number!!!!"
@@ -44,42 +87,50 @@ def do_image_magic(img, f):
         end_x = min(img.shape[1]-1, x+r)
 
         begin_fx = max(0, r-x)
-        end_fx = min(f.shape[1]-1, (img.shape[1] - x))
+        end_fx = min(f.shape[1]-1, (img.shape[1] -1  - x + r))
 
         tx = x if x <= r else r
-        print "x = %d, tx=%d" %(x, tx)
 
         for y in np.arange(0, img.shape[0]):
             begin_y = max(0, y-r)
-            end_y = min(img.shape[1]-1, y+r)
+            end_y = min(img.shape[0]-1, y+r)
 
             begin_fy = max(0, r-y)
-            end_fy = min(f.shape[0]-1, (img.shape[0] - y))
+            end_fy = min(f.shape[0]-1, (img.shape[0] - 1 - y + r))
 
             sub_filter = f[begin_fy:end_fy + 1, begin_fx:end_fx + 1]
             sub_img = img[begin_y:end_y + 1, begin_x:end_x + 1]
 
+            # print "calculate color at position = (%d, %d)" % (x, y)
             tmp[y, tx, :] = multiple_sum(sub_filter, sub_img)
 
         if x >= r:
-            print "copy pixels from (%d:%d, %d) to img(%d:%d, %d)" %(0, tmp.shape[0], 0, 0, img.shape[0], x-r)
             img[0:img.shape[0], x-r, :] = tmp[0:tmp.shape[0], 0, :]
             for i in np.arange(1, r+1):
                 tmp[0:tmp.shape[0], i-1, :] = tmp[0:tmp.shape[0], i, :]
 
     for x in np.arange(img.shape[1]-r, img.shape[1]):
-        print "copy pixels from (%d:%d, %d) to img(%d:%d, %d)" %(0, tmp.shape[0], img.shape[1]-x, 0, img.shape[0], x)
         img[0:img.shape[0], x, :] = tmp[0:tmp.shape[0], img.shape[1]-x, :]
 
     print '-----------do_image_magic-----------'
 
-imagefile = "flower.jpg"
-birdimage = cv2.imread(imagefile)
+imagefile = "lenna.jpg"
+image = cv2.imread(imagefile)
 
-cv2.imshow("original image", birdimage)
+i = 1
+for f in filters:
+    tmpimage = image.copy()
+    do_image_magic(tmpimage, f)
+    cv2.imshow("filter=" + str(i), tmpimage)
+    i += 1
 
-do_image_magic(birdimage, sharpness_filter_3_3)
+# cv2.imshow("image magic", image)
+#
+# do_image_magic(image, sharpness_filter_12)
+# do_image_magic(image, sharpness_filter_12)
+#
+# cv2.imshow("image magic show!!!![filter=", image)
 
-cv2.imshow("image magic show!!!!", birdimage)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
