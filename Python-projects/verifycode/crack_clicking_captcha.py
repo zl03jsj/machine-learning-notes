4d98b9d0da9c6729603ee190e9cedb776010960f
zl <zl03jsj@163.com>
2017-11-22 07:22:27 +0800

    修改边缘检测,增加cnnny算法.

--- Python-projects/verifycode/crack_clicking_captcha.py
+++ Python-projects/verifycode/crack_clicking_captcha.py
@@ -1,36 +1,39 @@
 # -*- coding:utf-8 -*-
-
 import cv2
-#from find_obj import filter_matches,explore_match
+# from find_obj import filter_matches,explore_match
 import numpy as np
 
-def filter_matches(kp1, kp2, matches, ratio = 0.75):
+res_path = 'img/origin2/'
+
+
+def filter_matches(kp1, kp2, matches, ratio=0.75):
     mkp1, mkp2 = [], []
     for m in matches:
         if len(m) == 2 and m[0].distance < m[1].distance * ratio:
             m = m[0]
-            mkp1.append( kp1[m.queryIdx] )
-            mkp2.append( kp2[m.trainIdx] )
+            mkp1.append(kp1[m.queryIdx])
+            mkp2.append(kp2[m.trainIdx])
     p1 = np.float32([kp.pt for kp in mkp1])
     p2 = np.float32([kp.pt for kp in mkp2])
     kp_pairs = zip(mkp1, mkp2)
     return p1, p2, kp_pairs
 
-def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
+
+def explore_match(win, img1, img2, kp_pairs, status=None, H=None):
     h1, w1 = img1.shape[:2]
     h2, w2 = img2.shape[:2]
-    vis = np.zeros((max(h1, h2), w1+w2), np.uint8)
+    vis = np.zeros((max(h1, h2), w1 + w2), np.uint8)
     vis[:h1, :w1] = img1
-    vis[:h2, w1:w1+w2] = img2
+    vis[:h2, w1:w1 + w2] = img2
     vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
 
     if H is not None:
         corners = np.float32([[0, 0], [w1, 0], [w1, h1], [0, h1]])
-        corners = np.int32( cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0) )
+        corners = np.int32(cv2.perspectiveTransform(corners.reshape(1, -1, 2), H).reshape(-1, 2) + (w1, 0))
         cv2.polylines(vis, [corners], True, (255, 255, 255))
 
     if status is None:
-        status = np.ones(len(kp_pairs), np.bool)
+        status = np.ones(len(list(kp_pairs)), np.bool)
     p1 = np.int32([kpp[0].pt for kpp in kp_pairs])
     p2 = np.int32([kpp[1].pt for kpp in kp_pairs]) + (w1, 0)
 
@@ -47,10 +50,10 @@ def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
             col = red
             r = 2
             thickness = 3
-            cv2.line(vis, (x1-r, y1-r), (x1+r, y1+r), col, thickness)
-            cv2.line(vis, (x1-r, y1+r), (x1+r, y1-r), col, thickness)
-            cv2.line(vis, (x2-r, y2-r), (x2+r, y2+r), col, thickness)
-            cv2.line(vis, (x2-r, y2+r), (x2+r, y2-r), col, thickness)
+            cv2.line(vis, (x1 - r, y1 - r), (x1 + r, y1 + r), col, thickness)
+            cv2.line(vis, (x1 - r, y1 + r), (x1 + r, y1 - r), col, thickness)
+            cv2.line(vis, (x2 - r, y2 - r), (x2 + r, y2 + r), col, thickness)
+            cv2.line(vis, (x2 - r, y2 + r), (x2 + r, y2 - r), col, thickness)
     vis0 = vis.copy()
     for (x1, y1), (x2, y2), inlier in zip(p1, p2, status):
         if inlier:
@@ -58,25 +61,26 @@ def explore_match(win, img1, img2, kp_pairs, status = None, H = None):
 
     cv2.imshow(win, vis)
 
-img1 = cv2.imread("E:\python\Python Project\opencv_showimage\images\multi_view_big.jpg")
-img2 = cv2.imread("E:\python\Python Project\opencv_showimage\images\multi_view_small.jpg")
+
+img1 = cv2.imread(res_path + 'sub_image_1.jpg')
+img2 = cv2.imread(res_path + 'sub_image_2.jpg')
 
 img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-sift = cv2.SIFT()
+# sift = cv2.SIFT()
+sift = cv2.xfeatures2d.SIFT_create()
 
-kp1,des1 = sift.detectAndCompute(img1_gray, None)
-kp2,des2 = sift.detectAndCompute(img2_gray, None)
+kp1, des1 = sift.detectAndCompute(img1_gray, None)
+kp2, des2 = sift.detectAndCompute(img2_gray, None)
 
 # BFmatcher with default parms
 bf = cv2.BFMatcher(cv2.NORM_L2)
 matches = bf.knnMatch(des1, des2, k=2)
 
-
-p1,p2,kp_pairs = filter_matches(kp1,kp2,matches,ratio=0.5)
-explore_match('matches', img1_gray,img2_gray,kp_pairs)
-#img3 = cv2.drawMatchesKnn(img1_gray,kp1,img2_gray,kp2,good[:10],flag=2)
+p1, p2, kp_pairs = filter_matches(kp1, kp2, matches, ratio=0.5)
+explore_match('matches', img1_gray, img2_gray, kp_pairs)
+# img3 = cv2.drawMatchesKnn(img1_gray,kp1,img2_gray,kp2,good[:10],flag=2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- Python-projects/verifycode/ege_detection.py
+++ Python-projects/verifycode/ege_detection.py
@@ -3,86 +3,74 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 from PIL import Image
 
+res_path = 'img/origin2/'
 
-def fun2(img):
-    mser = cv.MSER_create(_min_area=300)
+def show_canny():
+    low_threshold = 50
+    max_low_threshold = 255
+    ratio = 3
+    kernel_size = 3
+    img = cv.imread('img/origin2/002.jpg')
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    def canny_threshold(cur_threshold):
+        detected_edges = cv.GaussianBlur(gray, (3, 3), 0)
+        detected_edges = cv.Canny(detected_edges, cur_threshold, cur_threshold * ratio, apertureSize=kernel_size)
+        dst = cv.bitwise_and(img, img, mask=detected_edges)  # just add some colours to edges from original image.
+        cv.imshow('canny demo', dst)
+
+    cv.namedWindow('canny demo')
+    cv.createTrackbar('Min threshold', 'canny demo', low_threshold, max_low_threshold, canny_threshold)
+
+    canny_threshold(low_threshold)  # initialization
+    if cv.waitKey(0) == 27:
+        cv.destroyAllWindows()
+
+
+def canny__(img):
+    gray = cv.cvtColor(img, code=cv.COLOR_BGR2GRAY)
+    detected_edges = cv.GaussianBlur(gray, (3, 3), 0)
+    detected_edges = cv.Canny(detected_edges, 120, 120 * 3, apertureSize=3)
+    # just add some colours to edges from original image.
+    img = cv.bitwise_and(img, img, mask=detected_edges)
+    return img
+
+
+def mser_text_detect(img):
+    gray = cv.cvtColor(img, code=cv.COLOR_BGR2GRAY)
+    mser = cv.MSER_create(_min_area=250)
     regions, boxes = mser.detectRegions(gray)
     for box in boxes:
-        if box[2] > 60 or box[3] > 60: continue
         x, y, w, h = box
-        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    plt.imshow(img, 'brg')
-    plt.show()
-
-def fun1():
-    im = cv.imread('img/origin2/001.jpg', flags=cv.IMREAD_COLOR)
-    cv.imshow('original image', im)
-
-    thresholds = np.array(im)
-    cv.threshold(im, 120, 255, cv.THRESH_BINARY_INV, thresholds)
-    cv.imshow("thresholds", thresholds)
+        if w > 60 or w < 20 or h > 60 or h < 20: continue
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=1)
+    cv.imshow('final image', img)
     cv.waitKey()
 
+
+def laplace__(img):
+    thresholds = np.array(img)
+    cv.threshold(img, 110, 150, cv.THRESH_BINARY_INV, thresholds)
     original_to_gray = cv.cvtColor(thresholds, code=cv.COLOR_BGR2GRAY)
     gray = np.array(original_to_gray)
-    cv.imshow('original image convert to gray', gray)
-
     dst = cv.Laplacian(gray, ddepth=cv.CV_32FC3, ksize=3)
-    cv.imshow('gray laplaced to dst', dst)
-
     cv.convertScaleAbs(dst, gray)
-    cv.imshow('dst convert scale to gray', gray)
 
-    mser = cv.MSER_create(_min_area=70)
-    regions, boxes = mser.detectRegions(gray)
-    for box in boxes:
-        if (box[2] > 50 or box[2] <20) or (box[3] > 50 or box[3] <20) : continue
-        x, y, w, h = box
-        cv.rectangle(original_to_gray, (x, y), (x + w, y + h), (255, 0, 0), 2, )
-    cv.imshow('final image', original_to_gray)
+def do_ege_detect():
+    img = cv.imread('img/origin2/003.jpg', flags=cv.IMREAD_COLOR)
+    img = canny__(img)
+    gray = cv.cvtColor(img, code=cv.COLOR_BGR2GRAY)
+
+    sub_image = gray[0:50, 230:gray.shape[1]-10]
+    cv.imwrite(res_path + 'sub_image_1.jpg', sub_image)
+
+    sub_image= gray[50:gray.shape[0], 0:gray.shape[1]]
+    cv.imwrite(res_path + 'sub_image_2.jpg', sub_image)
+
+    cv.imshow('image', gray)
     cv.waitKey()
 
-#
-#
-fun1()
 
-# import cv2
-# import cv2.cv as cv
-#
-# def fun1():
-#     im = cv.LoadImage('img/origin2/002.jpg', cv.CV_LOAD_IMAGE_COLOR)
-#     # Laplace on a gray scale picture
-#     gray = cv.CreateImage(cv.GetSize(im), 8, 1)
-#     cv.CvtColor(im, gray, cv.CV_BGR2GRAY)
-#
-#     aperture = 3
-#
-#     dst = cv.CreateImage(cv.GetSize(gray), cv.IPL_DEPTH_32F, 1)
-#     cv.Laplace(gray, dst, aperture)
-#
-#     cv.ConvertScale(dst, gray)
-#     cv.ShowImage('dst convert to gray', gray)
-#
-#     # ------------------------------------
-#     # Laplace on color
-#     thresholded = cv.CloneImage(im)
-#     cv.Threshold(im, thresholded, 50, 255, cv.CV_THRESH_BINARY_INV)
-#     planes = [cv.CreateImage(cv.GetSize(im), 8, 1) for i in range(3)]
-#     laplace = cv.CreateImage(cv.GetSize(im), cv.IPL_DEPTH_16S, 1)
-#     colorlaplace = cv.CreateImage(cv.GetSize(im), 8, 3)
-#
-#     cv.Split(im, planes[0], planes[1], planes[2], None)  # Split channels to apply laplace on each
-#     for plane in planes:
-#         cv.Laplace(plane, laplace, 3)
-#         cv.ConvertScaleAbs(laplace, plane, 1, 0)
-#
-#     cv.Merge(planes[0], planes[1], planes[2], None, colorlaplace)
-#
-#     cv.ShowImage('Laplace Color', colorlaplace)
-#     # -------------------------------------
-#     cv.WaitKey(0)
-#
-#
-# fun1()
+if __name__ == '__main__':
+    # show_canny()
+    do_ege_detect()
